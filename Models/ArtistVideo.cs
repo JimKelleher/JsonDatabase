1b90bb1b539b7c958bdac1199c83bf2b57f3800b
James Kelleher <jim_kelleher@comcast.net>
2019-05-15 14:11:59 -0400

    first commit

---
+++ Models/ArtistVideo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Database.Models
+{
+    public class Video
+    {
+        public string videoId { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+        public int seconds { get; set; }
+    }
+
+    public class ArtistVideo
+    {
+        public string id { get; set; }
+        public List<Video> video { get; set; }
+    }
+
+}
---
+++ Models/RootObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Database.Models
+{
+    public class RootObject
+    {
+        public List<Artist> artist { get; set; }
+    }
+
+}
